6c635e9245099b0c2ff3c8c52a0a23c780a6a864
peacedude <victor_sanden@hotmail.com>
2017-02-10 14:47:11 +0100

    Add project files.

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 w = new Class1();
+            string[] words = File.ReadAllLines(@"C:\Users\Lexicon\Documents\Visual Studio 2015\Projects\ConsoleApplication5\Words\words.txt");
+            w.GetMenu();
+            Console.ReadKey(true);
+
+        }
+    }
+}
